cfba223bf2060696e8a22409e29659f08f1cffad
Furkan <furkan.tutal@outlook.com>
2021-05-07 17:34:14 +0300

    FifthDay
    5. Gün Ödev

--- fifthDay/src/Main.java
+++ fifthDay/src/Main.java
@@ -10,11 +10,11 @@ public class Main {
 		
 		
 		UserService userService = new UserManager(new InMemoryUserDao(),new GoogleLoggerManagerAdapter());
-	        userService.register(new User("Yusuf Enes","Aras","enesaras551@gmail.com","1234enes")); // Dogrulandi!
-		userService.register(new User("Yusuf Enes","Aras","enesaras551","1234enes")); // Yanlis girildi ve hata alindi.
+	        userService.register(new User("Furkan","Tutal","frkn75@gmail.com","75frkn75")); // Dogrulandi!
+		userService.register(new User("Fatih","Baycu","fatihbay@gmail.com","*/131/qwes")); // Yanlis girildi ve hata alindi.
 		
-		User user1 = new User("FURKAN","TUTAL","frknttl@gmail.com","qwer1234qwer");
-		User user2 = new User("FATÝH","BAYCU","fatbay@gmail.com","*131$qwea");
+		User user1 = new User("Furkan","Tutal","frkn75@gmail.com","75frkn75");
+		User user2 = new User("Fatih","Baycu","fatihbay@gmail.com","*/131/qwes");
 		userService.login(user1); //Google ile giris islemi yapýlýyor
 		userService.login(user2); // sisteme kayýt olmadýðý için hata ekraný.
 		userService.delete(user1); // silme islemi.
